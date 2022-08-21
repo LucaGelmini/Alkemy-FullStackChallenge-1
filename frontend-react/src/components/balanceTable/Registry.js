c8849956a0a32a9cf21936d421db499824e54a82
Luca <lucagelmini@github.com>
2022-08-21 16:58:18 -0300

    editable table ok

--- frontend-react/src/components/balanceTable/Registry.js
+++ frontend-react/src/components/balanceTable/Registry.js
@@ -1,38 +1,64 @@
-export default function Registry(props){
+import { useState } from "react"
+
+export default function Registry({data: {id,concept, amount,record_date,operationType}}){
     // console.log(props.data)
 
-
+    const [isEditable, setIsEditable] = useState(false)
     let registerValues = {
-        id: props.data.id,
-        concept: props.data.concept,
-        amount: props.data.amount,
-        record_date: props.data.record_date,
-        operationType: props.data.operationType.type
+        id: id,
+        concept: concept,
+        amount: amount,
+        record_date: record_date,
+        operationType: operationType.type
 
+    }
+
+    function editHandler(){
+        setIsEditable(!isEditable)
+    }
+    function dateTimeFormater(dateTime){
+        dateTime = dateTime.slice(0,19);
+        const [date, time] = dateTime.split('T');
+        const [year, month, day] = date.split('-');
+        return (`${day}/${month}/${year}, ${time}`)
     }
     return(
         <div className="balance-register-container">
             <div className="balance-register-main">
                 <div className="registry-amount-container">
-                    <div 
-                        className={`registry-table-amount ${registerValues.operationType === 'expenses'? 'expenses' : 'incomes'}`}
-                    >
-                        {registerValues.amount} $
-                    </div>
+                    {isEditable?
+                        <input type='numeric' defaultValue={registerValues.amount} className={'registry-amount-input'} ></input>:
+                        <div 
+                            className={`registry-table-amount ${registerValues.operationType === 'expenses'? 'expenses' : 'incomes'}`}
+                        >
+                            {registerValues.amount} $
+                        </div>
+                    }
                     <hr></hr>
                     <div>{registerValues.operationType}</div>
 
                 </div>
 
-                <div className="registry-concept">{registerValues.concept}</div>
+                <div className="registry-concept">
+                    {isEditable?
+                    <input type='text'defaultValue={registerValues.concept}></input>:
+                    <p>{registerValues.concept}</p>}
+                </div>
 
-                {console.log(registerValues.record_date.slice(0,19))}
-                <div className="registry-date"><input type='datetime-local' defaultValue={registerValues.record_date.slice(0,19)}/*'2018-06-12T19:30'*/></input></div>
+                <div className="registry-date">
+                    {isEditable?
+                    <input type='datetime-local' defaultValue={registerValues.record_date.slice(0,19)}></input>:
+                    <p>{dateTimeFormater(registerValues.record_date)}</p>}
+                    
+                </div>
             </div>
 
             <div className="registry-actions">
-                <div className="registry-action-button-edit">Edit</div>
                 <div className="registry-action-button-delete">Delete</div>
+                {isEditable?
+                <div className="registry-action-button-confirm" onClick={editHandler}>Confirm</div>:
+                <div className="registry-action-button-edit"  onClick={editHandler}>Edit</div>}
+                
             </div>
         </div>
     )
